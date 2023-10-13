d71fabc1470f1b34ddcaf80b09ff3626f9b39e65
Noah <noahhughesph@gmail.com>
2023-10-13 12:42:42 -0400

    done

--- server/app.py
+++ server/app.py
@@ -15,9 +15,11 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+
 @app.route('/')
 def index():
     return "Index for Game/Review/User API"
+
 
 @app.route('/games')
 def games():
@@ -39,10 +41,11 @@ def games():
 
     return response
 
+
 @app.route('/games/<int:id>')
 def game_by_id(id):
     game = Game.query.filter(Game.id == id).first()
-    
+
     game_dict = game.to_dict()
 
     response = make_response(
@@ -52,20 +55,51 @@ def game_by_id(id):
 
     return response
 
-@app.route('/reviews')
+
+@app.route('/reviews', methods=['GET', 'POST'])
 def reviews():
 
-    reviews = []
-    for review in Review.query.all():
-        review_dict = review.to_dict()
-        reviews.append(review_dict)
+    if (request.method == 'GET'):
+        return make_response([review.to_fict() for review in Review.auery.all()], 200)
 
-    response = make_response(
-        reviews,
-        200
-    )
+    elif (request.method == 'POST'):
+        new_review = Review(score=request.form.get('score'),
+                            comment=request.form.get('comment'),
+                            game_id=request.form.get('game_id'),
+                            user_id=request.form.get('user_id'))
 
-    return response
+        db.session.add(new_review)
+        db.session.commit()
+
+        return make_response(new_review.to_dict(), 201)
+
+
+app.route('/reviews/<int:id>', methods=['GET', 'DELETE', 'PATCH'])
+
+
+def review_by_id(id):
+    review = Review.query.filter_by(id=id).first()
+
+    if (review):
+        if (request.method == 'GET'):
+            return make_response(review.to_dict(), 200)
+
+        elif (request.method == 'DELETE'):
+            db.session.delete(review)
+            db.session.commit()
+            return make_response({"delete_successful": True, "message": "Review deleted."}, 201)
+
+        elif (request.method == 'PATCH'):
+            for attr in request.form:
+                setattr(review, attr, request.form.get(attr))
+
+            db.session.add(review)
+            db.commit()
+
+            return make_response(review.to_dict(), 200)
+    else:
+        pass
+
 
 @app.route('/users')
 def users():
@@ -81,6 +115,7 @@ def users():
     )
 
     return response
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
